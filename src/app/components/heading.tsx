9dd794f017e239faaea0b3c1cc4e2c95d32a0934
aisarthak <aisarthak2@gmail.com>
2024-11-07 12:33:20 +0530

    basic setup

---
+++ src/app/components/heading.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+
+interface HeadingProps { 
+    children: ReactNode
+    className?: string
+}
+
+
+
+export const Heading = ({ children, className , ...props}: HeadingProps) => {
+
+}

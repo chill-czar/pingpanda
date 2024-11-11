2d544d91913eb42eff51a4d15592d0e0240ab06e
aisarthak <aisarthak2@gmail.com>
2024-11-11 09:49:13 +0530

    random commit

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -2,6 +2,7 @@ import { MaxWidthWrapper } from "../../components/max-width-wrapper"
 import { Heading } from "../../components/heading"
 import { Check } from "lucide-react"
 import { ShinyButton } from "../../components/shiny-button"
+import { MockDiscordUI } from "../../components/mock-discord-ui"
 
 const Page = () => {
   return (
@@ -15,7 +16,7 @@ const Page = () => {
                 <br />
                 <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
                   Delivered to Your Discord
-                </span> 
+                </span>
               </Heading>
             </div>
             <p className="text-base/7 text-gray-600 max-w-prose text-center text-pretty">
@@ -25,11 +26,11 @@ const Page = () => {
                 sales, new sers , or any other event
               </span>{" "}
               sent directly to your Discord.
-            </p> 
-            <ul 
+            </p>
+            <ul
               className="space-y-2 text-base/7 text-gray-600 text-left
              flex flex-col sm:items-start"
-            > 
+            >
               {[
                 "Real-Time Discord alerts for critical events ",
                 "Buy once , user Forever",
@@ -52,7 +53,17 @@ const Page = () => {
           </div>
         </MaxWidthWrapper>
       </section>
-      <section></section>
+
+      <section className="relative bg-brand-25 pb-4">
+        <div className="absolute inset-x-0 bottom-24 top-24 bg-brand-700" />
+        <div className="relative mx-auto">
+          <MaxWidthWrapper className="relative">
+            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl">
+              <MockDiscordUI></MockDiscordUI>
+            </div>
+          </MaxWidthWrapper>
+        </div>
+      </section>
       <section></section>
       <section></section>
     </>
